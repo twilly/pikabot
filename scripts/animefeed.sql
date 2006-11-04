bf714be833c6f43019f99dcca81b2ae7c596c4b7
tristan.willy <tristan.willy@19a6c7a4-e520-0410-9843-bf830a7b7633>
2006-11-04 07:56:47 +0000

    missed a few email addresses

--- scripts/animefeed.sql
+++ scripts/animefeed.sql
@@ -1,5 +1,5 @@
 -- animefeed.sql: animefeed rss database schema
--- Copyright (C) 2006   Tristan Willy <tristan.willy@gmail.com>
+-- Copyright (C) 2006   Tristan Willy <tristan.willy at gmail.com>
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License version 2
