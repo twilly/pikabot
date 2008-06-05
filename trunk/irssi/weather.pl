7140801de187e9611ddda5a2392ec39d63e44e73
tristan.willy <tristan.willy@19a6c7a4-e520-0410-9843-bf830a7b7633>
2008-06-05 22:08:14 +0000

    additions: supports being ran outside of irssi and reports more weather data other tweaks include different error handling, whitespace, etc

--- trunk/irssi/weather.pl
+++ trunk/irssi/weather.pl
@@ -19,28 +19,52 @@ use strict;
 use Text::ParseWords;
 use LWP;
 use XML::DOM::Lite qw(Parser :constants);
-
 use vars qw($VERSION %IRSSI);
-use Irssi;
 $VERSION = '0.01';
 %IRSSI = ( 'authors'     => 'Tristan Willy',
            'contact'     => 'tristan.willy at gmail.com',
            'name'        => 'weather',
            'description' => 'Weather Channel trigger',
            'license'     => 'GPL v2' );
+my %weather_active_chans;
+my $errstr;
 
-Irssi::settings_add_str($IRSSI{'name'}, 'weather_channels', '');
+# If we're in irssi, load up irssi interface, otherwise call main
+if(in_irssi()){
+  load_irssi_internals();
+} else {
+  main();
+}
 
-my %deref_active_chans;
-load_globals();
 
-Irssi::signal_add('event privmsg', 'irc_privmsg');
-Irssi::signal_add('setup changed', 'load_globals');
+sub in_irssi {
+  eval { Irssi::Core::is_static() };
+  if($@){
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+
+sub load_irssi_internals  {
+  require Irssi;
+  Irssi->import;
+
+  Irssi::settings_add_str($IRSSI{'name'}, 'weather_channels', '');
+
+  load_globals();
+
+  Irssi::signal_add('event privmsg', 'irc_privmsg');
+  Irssi::signal_add('setup changed', 'load_globals');
+}
+
 
 sub load_globals {
-  map { $deref_active_chans{uc($_)} = 1 }
+  map { $weather_active_chans{uc($_)} = 1 }
     quotewords(',', 0, Irssi::settings_get_str('weather_channels'));
 }
+
 
 sub irc_privmsg {
   my ($server, $data, $from, $address) = @_;
@@ -48,7 +72,7 @@ sub irc_privmsg {
   my ($me, $target) = ($server->{'nick'}, find_target($to, $from));
 
   # Check if it was for the channel we joined...
-  if(uc($to) eq uc($me) or $deref_active_chans{uc($to)}){
+  if(uc($to) eq uc($me) or $weather_active_chans{uc($to)}){
     if($message =~ /^\s*!weather\s+(.+)\s*/i){
       trigger_weather($server, $target, $to, $from, $address, $1);
     }
@@ -57,25 +81,33 @@ sub irc_privmsg {
   return 1;
 }
 
+
+sub privmsg_error {
+  my ($server, $target) = @_; 
+  $server->command("msg $target $errstr");
+}
+
+
 sub trigger_weather {
   my ($server, $target, $to, $from, $address, $where) = @_;
 
-  my $loc_id = get_location_id($server, $target, $where) or return;
-  my $weather = get_weather($server, $target, $loc_id) or return;
-  $server->command("msg $target Weather near $weather->{location}: $weather->{temp}{F}°F ($weather->{temp}{C}°C) and $weather->{type}.");
+  my $loc_id = get_location_id($where) or return privmsg_error($server, $target);
+  my $weather = get_weather($loc_id) or return privmsg_error($server, $target);
+  $server->command("msg $target " . english_report($weather));
 }
 
+
 sub get_location_id {
-  my ($server, $target, $where) = @_;
+  my ($where) = @_;
   my $agent = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.1) Gecko/20060124 Firefox/1.5.0.1');
   my $search = $agent->get('http://xoap.weather.com/search/search?where=' . $where);
   if(not $search->is_success()){
-    $server->command("msg $target Failed to get search document. Broken service?");
-    return 
+    $errstr = "Failed to get search document. Broken service?";
+    return;
   } else {
     # find the ID
     my $search_doc = Parser->parse($search->content())
-      or do { $server->command("msg $target Failed to parse search result."); return };
+      or do { $errstr = "Failed to parse search result."; return };
     my ($hits, $loc) = (0, undef);
     map {
       if($_->tagName eq 'loc'){
@@ -84,7 +116,7 @@ sub get_location_id {
       }
     } @{$search_doc->documentElement()->childNodes()};
     if($hits < 1){
-      $server->command("msg $target Couldn't find \"$where\".");
+      $errstr = "Couldn't find \"$where\".";
       return;
     }
     if(defined $loc){
@@ -93,35 +125,58 @@ sub get_location_id {
   }
 }
 
+
 sub get_weather {
-  my ($server, $target, $loc_id) = @_;
+  my ($loc_id) = @_;
   my $agent = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.1) Gecko/20060124 Firefox/1.5.0.1');
   my $weather = $agent->get('http://xoap.weather.com/weather/local/' . $loc_id . '?cc=*');
   if(not $weather->is_success()){
-    $server->command("msg $target Failed to get search document. Broken service?");
+    $errstr = "Failed to get search document. Broken service?";
     return;
   } else {
     my $weather_doc = Parser->parse($weather->content());
     my $cc = undef;
     map { $cc = $_ if $_->tagName() eq 'cc' } @{$weather_doc->documentElement()->childNodes()};
     if(not defined $cc){
-      $server->command("msg $target Error getting local weather.");
+      $errstr = "Error getting local weather.";
       return;
     }
     my %actions = ( 'obst' => sub { $_[0]->{location} = $_[1] },
-                    'tmp'  => sub {
-                                $_[0]->{temp}{F} = $_[1];
-                                $_[0]->{temp}{C} = sprintf("%.2f", ($_[1] - 32) * (5.0 / 9.0));
-                              },
-                    't'    => sub { $_[0]->{type} = $_[1] }
+                    'tmp'  => sub { $_[0]->{temp}{F} = $_[1];
+                                    $_[0]->{temp}{C} = F_to_C($_[1]); },
+                    't'    => sub { $_[0]->{type} = $_[1] },
+                    'flik' => sub { $_[0]->{feelslike}{F} = $_[1];
+                                    $_[0]->{feelslike}{C} = F_to_C($_[1]); },
+                    'hmid' => sub { $_[0]->{humidity} = $_[1]; }
                   );
     my %result;
     map {
         $actions{$_->tagName()}->(\%result, $_->firstChild()->nodeValue(), $_) if defined $actions{$_->tagName()};
+        # print "no action for child node \"" . $_->tagName() . "\"\n" if not defined $actions{$_->tagName()};
     } @{$cc->childNodes()};
     return \%result;
   }
 }
+
+
+sub F_to_C {
+  return sprintf("%.1f", (shift() - 32) * (5.0 / 9.0));
+}
+
+
+sub english_report {
+  my $weather = shift;
+  my $report = "Weather near $weather->{location}: $weather->{temp}{F}°F ($weather->{temp}{C}°C)";
+  if(abs($weather->{temp}{F} - $weather->{feelslike}{F}) > 7){
+    $report .= ", but feels like $weather->{feelslike}{F}°F ($weather->{feelslike}{C}°C),";
+  }
+  $report .= " and $weather->{type}.";
+  if($weather->{humidity} > 60){
+    $report .= " Also, it's really humid out ($weather->{humidity}%).";
+  }
+  return $report;
+}
+
 
 sub find_target {
   my ($to, $from) = @_;
@@ -131,3 +186,23 @@ sub find_target {
   }
   return $from; # from a private message
 }
+
+
+sub main {
+  if($#ARGV < 0){
+    warn "I need arguments or to be loaded into irssi.\n";
+    exit 1;
+  }
+
+  my $location = '';
+  $location .= "$_ " foreach @ARGV;
+  chop($location);
+  my $loc_id = get_location_id($location) or die "cannot get location id";
+  my $weather = get_weather($loc_id) or die "cannot get weather";
+  use Data::Dumper;
+  print Dumper($weather);
+  print english_report($weather) . "\n";
+  exit 0;
+}
+
+
