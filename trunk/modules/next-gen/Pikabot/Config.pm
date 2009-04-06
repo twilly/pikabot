cae867244df65c35f953708e5ac5552abc619bf9
kool.name <kool.name@19a6c7a4-e520-0410-9843-bf830a7b7633>
2009-04-06 23:04:24 +0000

    In the process of re-writing next-gen.  I didn't like where the last one was going, it had some good things and as a great learning experience.
    Layout planned:
    | > driver
    |   |  > ../Modules/Pikabot.pm
    |   |    | > *::Reports.pm (holds various reporting strings & errors)
    |   |    | > *::Config.pm (holds configuration info)
    |   |    | > *::Trigger.pm (holds trigger structure stuff)
    |   |    | > *::Signal.pm (holds signal parsing stuff)
    |   |
    |   |  > driver/
    |        | > *.bm (bot components that will be compiled at runtime)
    |            |> */ (dir for stuff owned by *.bm)
    |----------------------------------------------------->
    
    Done:
      - Pikabot::Config is laid out, and ready to be fleshed out more if needed
      - Pikabot::Trigger
      - Pikabot::Reports is working, add error strings as needed
    
      - Pikabot.pm's config() method is "done" (beware evil hacks)
    
      - a driver demo (of what's coded) is provided in trunk/irssi/next-gen
    
    To do:
       - Finish Pikabot.pm's spawn() method
       - Code Pikabot::Signal
       - a lot of other stuff

---
+++ trunk/modules/next-gen/Pikabot/Config.pm
@@ -0,0 +1,86 @@
+#!/usr/bin/perl -w
+package Pikabot::Config;
+# Pikabot::Config: The cutest bot you've ever seen's config.
+#
+# Copyright (C) 2009  Justin Lee  < kool.name at gmail.com >
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+###
+# To do:
+#
+#   2009-04-06:
+#     - maybe replace this module with something else entirely?
+###
+# History:
+#
+#   2009-04-06:
+#     - added various fields
+#     - initial layout finished
+
+
+use strict;
+use warnings;
+
+use Pikabot::Reports qw(ERRSTR);
+
+#our (@ISA, @EXPORT_OK);
+#BEGIN {
+#  require Exporter;
+#  @ISA = qw(Exporter);
+#  @EXPORT_OK = qw(CONFIG);
+#}
+
+# inlined constants
+sub SECTION_NAME () { 'Config' }
+
+
+our (
+  $CONFIGED,
+
+  # Irssi settings_get_*() options
+  $IRSSI_SETTINGS_USE,
+  $IRSSI_SETTINGS_OVERLOAD_DRIVER,
+
+  # Vanity options
+  $BOT_NAME,
+  $BOT_VERSION,
+  $BOT_AUTHORS,
+
+  # Settings
+  $COMPONENT_DIRECTORY,
+  $GLOBAL_CHANNELS,
+) = (
+  0,
+
+  # Irssi settings_get_*() options
+  0,
+  0,
+
+  # Vanity options
+  'Pikabot',
+  '',
+  { # authors
+    'Justin Lee' => 'kool.name at gmail.com',
+    'Tristan Willy' => 'tristan.willy at gmail.com',
+    'Andreas Högström' => 'superjojo at gmail.com',
+  },
+
+  # Settings
+  undef,
+  [],
+);
+
+
+'Pikachu!';
