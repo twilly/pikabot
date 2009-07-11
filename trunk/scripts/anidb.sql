39de25e31f37f0e173a0661455341f9035bf4491
tristan.willy <tristan.willy@19a6c7a4-e520-0410-9843-bf830a7b7633>
2009-07-11 23:38:49 +0000

    Change anidb.sql back to v1 tables.
    This reverts back to 9918e6932c790624cbdb8c3ebcb1db1298a03baf.

--- trunk/scripts/anidb.sql
+++ trunk/scripts/anidb.sql
@@ -1,8 +1,6 @@
--- anidb.sql:
---   anidb database crap, now with less modular!
+-- anidb.sql: anidb database schema
 --
--- Copyright (C) 2006-2009  Tristan Willy  <tristan.willy at gmail.com>
--- Copyright (C) 2009  Justin "The Dean" Lee  <kool.name at gmail.com>
+-- Copyright (C) 2006,2009  Tristan Willy <tristan.willy at gmail.com>
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License version 2
@@ -15,211 +13,59 @@
 --
 -- You should have received a copy of the GNU General Public License
 -- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
--- USA.
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+-- Drop old data
+DROP TABLE anime;
+DROP TABLE titles;
+DROP TABLE genre_names;
+DROP TABLE genre;
+DROP TABLE details_cache;
+DROP TABLE search_cache_table;
+DROP TABLE search_hits;
+DROP VIEW  search_cache;
+VACUUM;
 
---
--- house keeping
-
-DROP TABLE IF EXISTS anime;
-
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS shortnames;
-DROP TABLE IF EXISTS synonyms;
-DROP TABLE IF EXISTS resources;
-
-DROP TABLE IF EXISTS tag;
-DROP TABLE IF EXISTS tags;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS creator;
-DROP TABLE IF EXISTS creators;
-
-DROP TABLE IF EXISTS results;
-DROP TABLE IF EXISTS query;
-
-DROP TABLE IF EXISTS config;
-
-DROP VIEW IF EXISTS animes;
-DROP VIEW IF EXISTS queries;
-
-DROP TRIGGER IF EXISTS refresh_animes;
-DROP TRIGGER IF EXISTS refresh_queries;
-
-
---
--- data tables
-
--- you win, pc486
+-- Data
 CREATE TABLE anime (
-  -- ident
-  aid INTEGER PRIMARY KEY NOT NULL,
-  title TEXT(1024) NOT NULL,
-  got DATE DEFAULT (julianday('now')),
-
-  -- info
-  type TEXT(1024) DEFAULT NULL,
-  length INTEGER DEFAULT NULL,
-  rating REAL DEFAULT NULL,
-  tmprating REAL DEFAULT NULL,
-  start DATE DEFAULT NULL,
-  end DATE DEFAULT NULL
+  aid INTEGER PRIMARY KEY,
+  type varchar(1024),
+  numeps INTEGER,
+  rating REAL,
+  startdate date,
+  enddate date,
+  url varchar(1024)
 );
-
-CREATE TABLE query (
-  qid INTEGER PRIMARY KEY AUTOINCREMENT,
-  term TEXT(1024) UNIQUE,
-  got DATE DEFAULT (julianday('now'))
-);
-
-CREATE TABLE results (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  qid INTEGER REFERENCES queries(qid),
-  title TEXT(1024),
-  aid INTEGER REFERENCES anime(aid)
-);
-
-CREATE TABLE creator (
-  pid INTEGER PRIMARY KEY,
-  type TEXT(1024) DEFAULT NULL,
-  title TEXT(1024)
-);
-
-CREATE TABLE creators (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  pid INTEGER REFERENCES creator(pid),
-  aid INTEGER REFERENCES anime(aid)
-);
-
-CREATE TABLE tag (
-  tid INTEGER PRIMARY KEY,
-  tag TEXT(1024),
-  description TEXT(1024) DEFAULT NULL -- reserved for later
-);
-
-CREATE TABLE tags (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  tid INTEGER REFERENCES tag(tid),
-  aid INTEGER REFERENCES anime(aid)
-);
-
-CREATE TABLE category (
-  cid INTEGER PRIMARY KEY,
-  category TEXT(1024),
-  description TEXT(1024) DEFAULT NULL -- reserved for later
-);
-
-CREATE TABLE categories (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  cid INTEGER REFERENCES categories(cid),
-  aid INTEGER REFERENCES anime(aid)
-);
-
 CREATE TABLE titles (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT(1024) UNIQUE ON CONFLICT REPLACE,
-  state INTEGER DEFAULT 0,
-  type TEXT(1024),
-  aid INTEGER REFERENCES anime(aid)
+  aid int REFERENCES anime(aid),
+  title varchar(1024)
+);
+CREATE TABLE genre_names (
+  gid INTEGER PRIMARY KEY AUTOINCREMENT,
+  gname varchar(1024) UNIQUE
+);
+CREATE TABLE genre (
+  aid int REFERENCES anime(aid),
+  gid int REFERENCES genre_names(gid)
 );
 
-CREATE TABLE synonyms (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  synonym TEXT(1024) UNIQUE ON CONFLICT REPLACE,
-  aid INTEGER REFERENCES anime(aid)
+-- Metadata
+CREATE TABLE details_cache (
+  aid int PRIMARY KEY REFERENCES anime(aid),
+  last_refreshed int
+);
+CREATE TABLE search_cache_table (
+    sid INTEGER PRIMARY KEY AUTOINCREMENT,
+    terms varchar(1024) UNIQUE,
+    last_refreshed DATE
+);
+CREATE TABLE search_hits (
+    sid int REFERENCES search_cache_table(sid),
+    id_num int CHECK (id_num >= 0),
+    title varchar(1024)
 );
 
-CREATE TABLE shortnames (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  shortname TEXT(1024) UNIQUE ON CONFLICT REPLACE,
-  aid INTEGER REFERENCES anime(aid)
-);
-
-CREATE TABLE resources (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT(1024) NOT NULL ON CONFLICT IGNORE,
-  link TEXT(1024) NOT NULL ON CONFLICT IGNORE,
-  aid INTEGER REFERENCES anime(aid)
-);
-
-
---
--- Meta crap
-
-CREATE TABLE config (
-  name TEXT(1024) PRIMARY KEY NOT NULL,
-  value BLOB(1024)
-);
-
--- 30 day timeout on scraped data
-INSERT INTO config VALUES('anime_refresh', 30);
-
--- 10 day timeout on queries
-INSERT INTO config VALUES('query_refresh', 10);
-
-
---
--- Management stuff! :D
-
--- these next two are for quickly checking the DB for valid anime/query info
-CREATE VIEW animes AS
-  SELECT * FROM anime
-    WHERE
-      (got + (SELECT value FROM config WHERE name = 'anime_refresh'))
-        >
-      julianday('now');
-
-CREATE VIEW queries AS
-  SELECT * FROM query
-    WHERE
-      (got + (SELECT value FROM config WHERE name = 'query_refresh'))
-        >
-      julianday('now');
-
--- trigger for purging expired anime info!
-CREATE TRIGGER purge_animes
-  INSTEAD OF DELETE ON animes
-    BEGIN
-      DELETE FROM anime
-        WHERE
-          (got + (SELECT value FROM config WHERE name = 'anime_refresh') - 1)
-            <
-          julianday('now');
-      -- This section could possibly be sped up?
-      DELETE FROM creators
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = creators.aid);
-      DELETE FROM tags
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = tags.aid);
-      DELETE FROM categories
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = categories.aid);
-      DELETE FROM titles
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = titles.aid);
-      DELETE FROM shortnames
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = shortnames.aid);
-      DELETE FROM synonyms
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = synonyms.aid);
-      DELETE FROM resources
-        WHERE
-          NOT EXISTS (SELECT * FROM anime WHERE anime.aid = resources.aid);
-    END;
-
--- trigger for purging expired queries
-CREATE TRIGGER purge_queries
-  INSTEAD OF DELETE ON queries
-    BEGIN
-      DELETE FROM query
-        WHERE
-          (got + (SELECT value FROM config WHERE name = 'query_refresh') - 1)
-            <
-          julianday('now');
-      DELETE FROM results
-        WHERE
-          NOT EXISTS (SELECT * FROM query WHERE query.qid = results.qid);
-    END;
+-- Use this to inspect the search cache
+CREATE VIEW search_cache AS
+  SELECT * FROM search_cache_table
+    WHERE (last_refreshed + 30) > julianday('now');
