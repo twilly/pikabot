04df134d74f9cbdb2d0e274c20110973c1ec4328
tristan.willy <tristan.willy@19a6c7a4-e520-0410-9843-bf830a7b7633>
2011-12-31 23:05:37 +0000

    Move from Weather Channel to Weather Underground

--- trunk/irssi/weather.pl
+++ trunk/irssi/weather.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
-# weather.pl: Weather Channel weather information.
+# weather.pl: weather information.
 #
-# Copyright (C) 2007   Tristan Willy <tristan.willy at gmail.com>
+# Copyright (C) 2007-2011 Tristan Willy <tristan.willy at gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 2
@@ -19,17 +19,17 @@
 use strict;
 use Text::ParseWords;
 use LWP;
-use XML::DOM;
+use JSON;
 use utf8;
 use vars qw($VERSION %IRSSI);
 $VERSION = '0.01';
 %IRSSI = ( 'authors'     => 'Tristan Willy',
            'contact'     => 'tristan.willy at gmail.com',
            'name'        => 'weather',
-           'description' => 'Weather Channel trigger',
+           'description' => 'Weather Underground trigger',
            'license'     => 'GPL v2' );
 my %weather_active_chans;
-my ($errstr, $bot_pid, $bot_key);
+my ($errstr, $bot_key);
 
 # If we're in irssi, load up irssi interface, otherwise call main
 if(in_irssi()){
@@ -54,7 +54,6 @@ sub load_irssi_internals  {
   Irssi->import;
 
   Irssi::settings_add_str($IRSSI{'name'}, 'weather_channels', '');
-  Irssi::settings_add_str($IRSSI{'name'}, 'weather_partner_id', '');
   Irssi::settings_add_str($IRSSI{'name'}, 'weather_key', '');
 
   load_globals();
@@ -67,11 +66,9 @@ sub load_irssi_internals  {
 sub load_globals {
   map { $weather_active_chans{uc($_)} = 1 }
     quotewords(',', 0, Irssi::settings_get_str('weather_channels'));
-  $bot_pid = Irssi::settings_get_str('weather_partner_id');
   $bot_key = Irssi::settings_get_str('weather_key');
-  if(not $bot_pid or not $bot_key){
-    Irssi::print("Warning: weather script requires partner_id and " .
-        "key settings");
+  if(not $bot_key){
+    Irssi::print("Warning: weather script requires an API key");
   }
 }
 
@@ -103,86 +100,69 @@ sub trigger_weather {
 
   my $loc_id = get_location_id($where)
     or return privmsg_error($server, $target);
-  my $weather = get_weather($loc_id, $bot_pid, $bot_key)
+  my $weather = get_weather($loc_id, $bot_key)
     or return privmsg_error($server, $target);
   $server->command("msg $target " . english_report($weather));
 }
 
 
 sub get_location_id {
-  my ($where) = @_;
-  my $agent = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.1) Gecko/20060124 Firefox/1.5.0.1');
-  my $search = $agent->get('http://xoap.weather.com/search/search?where=' . $where);
-  if(not $search->is_success()){
-    $errstr = "Failed to get search document. Broken service?";
-    return;
-  } else {
-    # find the ID
-    my $parser = new XML::DOM::Parser;
-    my $search_doc = $parser->parse($search->content())
-      or do { $errstr = "Failed to parse search result."; return };
-    my ($hits, $loc) = (0, undef);
-    map {
-      if($_->getNodeName() eq 'loc'){
-        $hits++;
-        $loc = $_ if not defined $loc;
-      }
-    } @{$search_doc->getDocumentElement()->getChildNodes()};
-    if($hits < 1){
-      $errstr = "Couldn't find \"$where\".";
-      return;
+    my ($where) = @_;
+    my $agent = LWP::UserAgent->new or die;
+    my $search =
+        $agent->get('http://autocomplete.wunderground.com/aq?query=' .
+                $where . '&format=JSON');
+    if(not $search->is_success()){
+        $errstr = "Failed to get search document: " . $search->status_line;
+        return;
+    } else {
+        # find the ID
+        my $json = JSON->new or die;
+        my $doc = $json->decode($search->content())
+            or do { $errstr = "Failed to parse search result."; return };
+        if(defined $doc->{RESULTS}->[0]->{l}){
+            return $doc->{RESULTS}->[0]->{l};
+        } else {
+            $errstr = "Cannot find $where.";
+            return;
+        }
     }
-    if(defined $loc){
-      return $loc->getAttribute("id");
-    }
-  }
 }
 
 
 sub get_weather {
-  my ($loc_id, $pid, $key) = @_;
-  my $agent = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.1) Gecko/20060124 Firefox/1.5.0.1');
-  my $weather = $agent->get('http://xoap.weather.com/weather/local/' .
-          $loc_id . "?link=xoap&prod=xoap&par=$pid&key=$key&cc=");
-  if(not $weather->is_success()){
-    $errstr = "Failed to get search document. Broken service?";
-    return;
-  } else {
-    my $parser = new XML::DOM::Parser;
-    my $weather_doc = $parser->parse($weather->content());
-    my ($cc, $err);
-    foreach (@{$weather_doc->getDocumentElement()->getChildNodes()}){
-        if($_->getNodeName() eq 'cc'){
-            $cc = $_;
-        }
-        if($_->getNodeName() eq 'err'){
-            $err = $_;
-        }
-    }
-    if(not defined $cc){
-        if(defined $err){
-            $errstr = $err->getFirstChild->getNodeValue();
+    my ($loc_id, $key) = @_;
+    my $agent = LWP::UserAgent->new or die;
+    my $weather =
+        $agent->get("http://api.wunderground.com/api/$key/conditions/$loc_id.json");
+    if(not $weather->is_success()){
+        $errstr = "Failed to get weather document: " . $weather->status_line;
+        return;
+    } else {
+        my $json = JSON->new or die;
+        my $doc = $json->decode($weather->content())
+            or do { $errstr = "Failed to parse search result."; return };
+        my $curobs = $doc->{current_observation};
+        my %result = (
+                'location' => $curobs->{display_location}->{full},
+                'temp' => { 'F' => $curobs->{temp_f} },
+                'type' => $curobs->{weather},
+                'humidity' => $curobs->{relative_humidity},
+                );
+
+        # feels-like is windchill. if it doesn't exist, then fill with current temp.
+        if($curobs->{windchill_f} ne 'NA'){
+            $result{feelslike}{F} = $curobs->{windchill_f};
         } else {
-            $errstr = "Error getting local weather.";
+            $result{feelslike}{F} = $result{temp}{F};
         }
-      return;
+
+        # convert units
+        $result{temp}{C} = F_to_C($result{temp}{F});
+        $result{feelslike}{C} = F_to_C($result{feelslike}{F});
+
+        return \%result;
     }
-    my %actions = ( 'obst' => sub { $_[0]->{location} = $_[1] },
-                    'tmp'  => sub { $_[0]->{temp}{F} = $_[1];
-                                    $_[0]->{temp}{C} = F_to_C($_[1]); },
-                    't'    => sub { $_[0]->{type} = $_[1] },
-                    'flik' => sub { $_[0]->{feelslike}{F} = $_[1];
-                                    $_[0]->{feelslike}{C} = F_to_C($_[1]); },
-                    'hmid' => sub { $_[0]->{humidity} = $_[1]; }
-                  );
-    my %result;
-    map {
-        if(defined $actions{$_->getNodeName()}){
-            $actions{$_->getNodeName()}->(\%result, $_->getFirstChild()->getNodeValue(), $_)
-        }
-    } @{$cc->getChildNodes()};
-    return \%result;
-  }
 }
 
 
@@ -217,24 +197,21 @@ sub find_target {
 
 
 sub main {
-  if($#ARGV < 2){
+  if($#ARGV < 1){
     warn "I need arguments or to be loaded into irssi.\n";
-    warn "Usage: $0 [partner ID] [key] [location]\n";
+    warn "Usage: $0 [key] [location]\n";
     exit 1;
   }
 
-  my $pid = shift @ARGV;
   my $key = shift @ARGV;
 
   my $location = '';
   $location .= "$_ " foreach @ARGV;
   chop($location);
   my $loc_id = get_location_id($location)
-      or die "cannot get location id for \"$location\"";
-  my $weather = get_weather($loc_id, $pid, $key)
+      or die "cannot get location id for \"$location\": $errstr";
+  my $weather = get_weather($loc_id, $key)
       or die "cannot get weather for location $loc_id: $errstr\n";
-  use Data::Dumper;
-  print Dumper($weather);
   print english_report($weather) . "\n";
   exit 0;
 }
